7d3a479308f9c2911b7f0be9da20b2ef37ee566c
Anthony Leach <anthony.leach@hogarthww.com>
2020-10-31 16:19:38 +0000

    Infra: When single nat instance is used, all private subnets require a route to the nat instance.

--- nat-instance.tf
+++ nat-instance.tf
@@ -118,9 +118,9 @@ resource "aws_eip_association" "nat_instance" {
 }
 
 resource "aws_route" "nat_instance" {
-  count                  = var.nat_instance_single ? 1 : local.enabled ? local.nat_instance_count : 0
+  count                  = local.enabled ? length(aws_route_table.private) : 0
   route_table_id         = element(aws_route_table.private.*.id, count.index)
-  instance_id            = element(aws_instance.nat_instance.*.id, count.index)
+  instance_id            = var.nat_instance_single ? aws_instance.nat_instance[0].id : element(aws_instance.nat_instance.*.id, count.index)
   destination_cidr_block = "0.0.0.0/0"
   depends_on             = [aws_route_table.private]
 
